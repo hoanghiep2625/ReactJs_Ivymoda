bf6dbc1d6201252e46f7ce04b41ba527388ffed5
hoanghiep2625 <hoanghiep262005@gmail.com>
2025-04-25 10:34:07 +0700

    update vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,28 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     include: ["swiper"],
   },
   server: {
-    host: "0.0.0.0",
-    port: 5173,
+    host: "0.0.0.0",  // Chạy trên tất cả các địa chỉ IP
+    port: 5173,  // Port của dev server
     allowedHosts: [
-      "ivymoda.tahoanghiep.com",
-      "admin.ivymoda.tahoanghiep.com", // Thêm để hỗ trợ cả server admin
+      "ivymoda.tahoanghiep.com", // Thêm domain của bạn vào đây
     ],
+    hmr: {
+      protocol: "wss",  // Dùng wss cho HTTPS
+      host: "ivymoda.tahoanghiep.com",  // Sử dụng domain thật của bạn
+      port: 443,  // Port 443 cho HTTPS
+    },
   },
   preview: {
     host: "0.0.0.0",
     port: 5173,
     allowedHosts: [
       "ivymoda.tahoanghiep.com",
-      "admin.ivymoda.tahoanghiep.com", // Thêm để hỗ trợ cả server admin
     ],
   },
 });
